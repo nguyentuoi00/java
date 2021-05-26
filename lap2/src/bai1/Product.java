0ba7a0a4e1bc9a263cb749c93d2f59fd2c7b257c
Admin <Admin@DESKTOP-HV2USD0>
2021-05-26 22:13:36 +0700

    tuoi commit

---
+++ lap2/src/bai1/Product.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai1;
+
+/**
+ *
+ * @author Admin
+ */
+public class Product {
+
+    private String tenHangHoa;
+    private String nhaSanXuat;
+    private float giaBan;
+
+    public Product(String tenHangHoa, String nhaSanXuat, float giaBan) {
+        this.tenHangHoa = tenHangHoa;
+        this.nhaSanXuat = nhaSanXuat;
+        this.giaBan = giaBan;
+    }
+
+}
