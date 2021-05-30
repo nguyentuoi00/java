cb779d4dbf45dc934b5e043206c5a67446a517f1
Admin <Admin@DESKTOP-HV2USD0>
2021-05-30 23:47:05 +0700

    Commit

--- Lab11 (1)/src/Bai7/URLReader.java
+++ Lab11 (1)/src/Bai7/URLReader.java
@@ -7,20 +7,21 @@ package Bai7;
 
 /**
  *
-
+ *
  */
 import java.net.*;
 import java.io.*;
-public class URLReader 
-{
- public static void main(String[] args) throws Exception 
- {
- URL oracle = new URL("http://www.oracle.com/");
- BufferedReader in = new BufferedReader(
- new InputStreamReader(oracle.openStream()));
- String inputLine;
- while ((inputLine = in.readLine()) != null)
- System.out.println(inputLine);
- in.close();
- }
+
+public class URLReader {
+
+    public static void main(String[] args) throws Exception {
+        URL oracle = new URL("http://www.oracle.com/");
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(oracle.openStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine);
+        }
+        in.close();
+    }
 }
